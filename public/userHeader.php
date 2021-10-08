a875890d1ca4e07b5bd397e4665c5cc27289d552
DESKTOP-U4QV2RG\randa <randallgears1998@gmail.com>
2021-10-08 15:25:14 -0600

    Menu superior adaptativo

--- public/userHeader.php
+++ public/userHeader.php
@@ -9,4 +9,29 @@
     <link href="public/css/base.css" rel="stylesheet">
   </head>
   <body>
-    <h1 class="text-primary">Header</h1>
+    <header class="bg-info">
+      <section class="container-fluid">
+        <div class="row">
+          <div class="col-12 col-sm-3 col-lg-3">
+              <h3 class="text-white font-weight-bold" id="menuTitle">DIGITALIZATE</h3>
+          </div>
+          <div class="col-12 col-sm-9 col-lg-9">
+              <ul class="d-flex justify-content-around" id="menuUl">
+                <a href="#" class="btn text-white">Principal</a>
+                <div class="dropdown show">
+                  <a class="btn dropdown-toggle text-white" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Categorías
+                  </a>
+
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <a class="dropdown-item" href="#">Alfabetización</a>
+                    <a class="dropdown-item" href="#">Ciberseguridad</a>
+                  </div>
+                </div>
+                <a href="#" class="btn text-white">¿Como funciona?</a>
+                <a href="#" class="btn text-white">Quienes somos</a>
+              </ul>
+          </div>
+        </div>
+      </section>
+    </header>
